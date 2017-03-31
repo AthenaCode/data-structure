cea71c6ffae126e4d9e7df52adb076d0f42897e1
Rachel <racheldepriest1@gmail.com>
2017-03-31 17:05:31 -0500

    finished functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = Object.keys(storage).length = 0;
+  var end = Object.keys(storage).length = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[end] = value;
+    end += 1;
   };
 
   someInstance.dequeue = function() {
+    start += 1;
+    return storage[start -1];
+
   };
 
   someInstance.size = function() {
+    if(end - start < 0){
+      return 0;
+    } 
+    return end - start;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,7 +3,8 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  
+  
   // Implement the methods below
   someInstance.push = function(value) {
   };
