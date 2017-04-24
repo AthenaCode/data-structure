c9742c3cfc8ce6cb009f9c89589fb1ade1cd92ee
Rachel <racheldepriest1@gmail.com>
2017-04-23 23:49:12 -0500

    deleted files

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+	// Hey! Rewrite in the new style. Your code will wind up looking very similar,
+	// but try not not reference your old code in writing the new style.
+
+	var instance = {
+		begin: 0,
+		end: 0,
+		storage: {}
+	}
+	_.extend(instance, stackMethods);
+	return instance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value) {
 
+		this.end += 1;
+	},
+	pop: function() {
+		this.end -= 1;
+	},
+	size: function() {
 
+	}
+};
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,23 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  
-  
+  var start = Object.keys(storage).length = 0;
+  var end = Object.keys(storage).length = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[start] = value;
+    end++;
   };
 
   someInstance.pop = function() {
+    end--;
+    return storage[start]
   };
 
   someInstance.size = function() {
+    if(end - start < 0) return 0;
+    return end - start;
   };
 
   return someInstance;
